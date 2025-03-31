b8508678f2c2875b5ea6ef77b0bf9c487eabf8be
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-04-01 00:22:23 +0530

    update parser

--- parser.js
+++ parser.js
@@ -1,11 +1,29 @@
 module.exports = function parser(tokens) {
     let current = 0;
+
     function walk() {
         let token = tokens[current];
+
         if (token.type === 'number') {
             current++;
             return {
                 type: 'NumberLiteral',
+                value: token.value
+            };
+        }
+
+        if (token.type === 'string') {
+            current++;
+            return {
+                type: 'StringLiteral',
+                value: token.value
+            };
+        }
+
+        if (token.type === 'boolean') {
+            current++;
+            return {
+                type: 'BooleanLiteral',
                 value: token.value
             };
         }
