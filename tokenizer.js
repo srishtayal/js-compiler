6b30615c69718648bdfb988972a728999cc060ff
Srishti Tayal <168971159+srishtayal@users.noreply.github.com>
2025-04-01 00:20:34 +0530

    update tokenizer

--- tokenizer.js
+++ tokenizer.js
@@ -6,9 +6,10 @@ const OPERATORS = /[+\-*\/]/;
 module.exports = function tokenizer(input) {
     const tokens = [];
     let current = 0;
-    
+
     while (current < input.length) {
         let char = input[current];
+
         if (char === '(' || char === ')') {
             tokens.push({ type: 'paren', value: char });
             current++;
@@ -26,6 +27,41 @@ module.exports = function tokenizer(input) {
         }
 
         if (WHITESPACE.test(char)) {
+            current++;
+            continue;
+        }
+
+        if (char ==='"' || char === "'") {
+            let value = '';
+            let quoteType = char;
+            char = input[++current];
+            while (char !== quoteType) {
+                value += char;
+                char = input[++current];
+            }
+            current++;
+            tokens.push({ type: 'string', value });
+            continue;
+        }
+
+        if (input.slice(current, current + 4) === 'true') {
+            tokens.push({ type: 'boolean', value: true });
+            current += 4;
+            continue;
+        }
+
+        if (input.slice(current, current + 5) === 'false') {
+            tokens.push({ type: 'boolean', value: false });
+            current += 5;
+            continue;
+        }
+
+        if (OPERATORS.test(char)) {
+            let value = char;
+            if (["=", ">", "<", "!"].includes(char) && input[current + 1] === "=") {
+                value += input[++current];
+            }
+            tokens.push({ type: 'operator', value });
             current++;
             continue;
         }
